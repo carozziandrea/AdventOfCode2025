cc1731419b3d42e02adc18589e39f6ed50c0cc81
Andrea Carozzi <carozzi.andrea@gmail.com>
2025-12-01 11:28:20 +0100

    Day 01 full solution

--- Day01/day01.cpp
+++ Day01/day01.cpp
@@ -17,15 +17,36 @@ int main(int argc, char const *argv[])
     int dial_size = 100;
     int dial = 50;
     int zeroes = 0;
+    int part2zeroes = 0;
 
     while(getline(f, line)){
         //std::cout << line << std::endl;
         char dir = line[0];
         int rot = std::stoi(line.substr(1, line.size()-1));
         if (dir == 'L'){
-            dial = (((dial - rot) % dial_size) + dial_size) % dial_size;
+            while(rot != 0){
+                dial--;
+                if(dial == -1){
+                    dial = 99;
+                }
+                if(dial == 0){
+                    part2zeroes++;
+                }
+                rot--;
+            }
+            //dial = (((dial - rot) % dial_size) + dial_size) % dial_size;
         }else{
-            dial = (((dial + rot) % dial_size) + dial_size) % dial_size;
+            while(rot != 0){
+                dial++;
+                if(dial == 100){
+                    dial = 0;
+                }
+                if(dial == 0){
+                    part2zeroes++;
+                }
+                rot--;
+            }
+            //dial = (((dial + rot) % dial_size) + dial_size) % dial_size;
         }
         //std::cout << '\t'<< dial << std::endl;
         if(dial == 0){
@@ -34,6 +55,7 @@ int main(int argc, char const *argv[])
     }
 
     std::cout << "Part 1: " << zeroes << std::endl;
+    std::cout << "Part 2: " << part2zeroes << std::endl;
     f.close();
 
     return 0;
