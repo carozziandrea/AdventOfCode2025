9341770c84971cdeb3c6665e24cde508d335b886
Andrea Carozzi <carozzi.andrea@gmail.com>
2025-12-03 14:52:38 +0100

    Day 03 full solution

--- Day03/day03.py
+++ Day03/day03.py
@@ -12,3 +12,32 @@ with open('Day03/day03input.txt', 'r') as lines:
         total_jolts += int(jolts)
 
 print(f'Part 1: {total_jolts}')
+
+def get_largest_subsequence(digits_list, k):
+    stack = []
+    drop_count = len(digits_list) - k
+    
+    for digit in digits_list:
+        while stack and digit > stack[-1] and drop_count > 0:
+            stack.pop()
+            drop_count -= 1
+        stack.append(digit)
+        
+    return stack[:k]
+
+total_sum = 0
+with open('Day03/day03input.txt', 'r') as lines:
+    for line in lines:
+        line = line.strip()
+        if not line: continue
+        
+        bank = [int(x) for x in list(line)]
+        
+        result_list = get_largest_subsequence(bank, 12)
+        
+        result_string = "".join(map(str, result_list))
+        result_num = int(result_string)
+        
+        total_sum += result_num
+
+print(f"Part 2: {total_sum}")
