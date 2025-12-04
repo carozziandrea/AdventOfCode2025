22c054b3a75d4e24a0f042867bf3c105ddc46e1b
Andrea Carozzi <carozzi.andrea@gmail.com>
2025-12-04 12:41:28 +0100

    Day 04 full solution

--- Day04/day04.py
+++ Day04/day04.py
@@ -1,50 +1,44 @@
-def print_grid(grid):
-    for line in grid:
-        print(''.join(line))
-
-def checkAdjacent(grid, r, c):
-    dirs = {(-1, -1), (-1, 0), (-1, 1),
+def should_be_removed(grid, r, c):
+    dirs = [(-1, -1), (-1, 0), (-1, 1),
             (0, -1), (0, 1),
-            (1, -1), (1, 0), (1, 1)}
-    
+            (1, -1), (1, 0), (1, 1)]
+
     rows_size = len(grid)
     cols_size = len(grid[0])
 
-    rolls = 0
-    
-    for dir in dirs:
-        newr = r + dir[0]
-        newc = c + dir[1]
-        if newr < 0 or newr >= rows_size: continue
-        if newc < 0 or newc >= cols_size: continue
+    neighbor_count = 0
 
-        if grid[newr][newc] == '@': rolls += 1
+    for dr, dc in dirs:
+        newr, newc = r + dr, c + dc
+        if 0 <= newr < rows_size and 0 <= newc < cols_size:
+            if grid[newr][newc] == '@':
+                neighbor_count += 1
+                if neighbor_count >= 4:
+                    return False
 
-    return rolls < 4
+    return True
 
 # Part 1 and 2
-oldGrid = []
-newGrid = []
-with open('Day04/day04input.txt', 'r') as lines:
-    for line in lines:
-        oldGrid.append(list(line.strip()))
+with open('Day04/day04input.txt', 'r') as f:
+    grid = [list(line.strip()) for line in f]
 
 removedRolls = 0
-currentRemovedRolls = (len(oldGrid) * len(oldGrid[0]))
+currentRemovedRolls = 1
 first_removal = True
+
 while currentRemovedRolls != 0:
     currentRemovedRolls = 0
     to_remove = []
 
-    for r_index, r in enumerate(oldGrid):
-        for c_index, c in enumerate(r):
-            if oldGrid[r_index][c_index] != '@': continue
-            if checkAdjacent(oldGrid, r_index, c_index):
+    for r_index, row in enumerate(grid):
+        for c_index, cell in enumerate(row):
+            if cell != '@': continue
+            if should_be_removed(grid, r_index, c_index):
                 currentRemovedRolls += 1
                 to_remove.append((r_index, c_index))
 
     for r, c in to_remove:
-        oldGrid[r][c] = '.'
+        grid[r][c] = '.'
 
     if first_removal:
         print(f'Part 1: {currentRemovedRolls}')
